6065b12d6ef12aabefe730373f33eedd5ebf6c79
Jimmy Li <Jimmy.Li@msn.com>
2021-01-04 23:35:39 +0800

    3rd: Learn `creation` in rxjs (#14)

--- src/app/demo/rxjs/rxjs.component.spec.ts
+++ src/app/demo/rxjs/rxjs.component.spec.ts
@@ -3,23 +3,23 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { RxjsComponent } from './rxjs.component';
 
 describe('RxjsComponent', () => {
-  let component: RxjsComponent;
-  let fixture: ComponentFixture<RxjsComponent>;
+    let component: RxjsComponent;
+    let fixture: ComponentFixture<RxjsComponent>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      declarations: [ RxjsComponent ]
-    })
-    .compileComponents();
-  });
+    beforeEach(async () => {
+        await TestBed.configureTestingModule({
+            declarations: [RxjsComponent]
+        })
+            .compileComponents();
+    });
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(RxjsComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(RxjsComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
--- src/app/demo/rxjs/rxjs.component.ts
+++ src/app/demo/rxjs/rxjs.component.ts
@@ -1,19 +1,69 @@
 import { Component, OnInit } from '@angular/core';
+import { from, fromEvent, interval, Observable, of } from "rxjs";
 
+// Model
+import { IVm } from 'src/app/model/vm';
+
+// Service
 import { IVmService } from "../../service/i-vm.service";
 
 @Component({
-  selector: 'app-rxjs',
-  templateUrl: './rxjs.component.html',
-  styleUrls: ['./rxjs.component.css']
+    selector: 'app-rxjs',
+    templateUrl: './rxjs.component.html',
+    styleUrls: ['./rxjs.component.css']
 })
 export class RxjsComponent implements OnInit {
 
-  constructor(
-    private vmService: IVmService
-  ) { }
+    private vms: IVm[];
 
-  ngOnInit(): void {
-  }
+    private vms1$: Observable<IVm[]>;
 
+    constructor(
+        private vmService: IVmService
+    ) { }
+
+    ngOnInit(): void {
+        this.vms = this.vmService.getAllWithOutObservable();
+    }
+
+    // creation with of
+    createWithOf(): void {
+        this.vms1$ = of(this.vms);
+
+        this.vms1$.subscribe(vms => {
+            window.console.log(vms);
+            debugger;
+
+        }, error=> {
+            window.console.log(error);
+        }, () => {
+            window.console.log('complete');
+        });
+    }
+
+    // creation with interval
+    createWithInterval(): void {
+        const valueForInterval: Observable<Number> = interval(1000);
+
+        valueForInterval.subscribe(n => {
+            window.console.log(n);
+        })
+    }
+
+    // creation with from
+    // createWithFrom(): void {
+    // TBD
+    // }
+
+    createWithFromEvent(): void {
+        const testElement = document.querySelector('#test');
+
+        const inputChangeEvent$ = fromEvent(testElement, 'change');
+        const inputChangeEventSubScription = inputChangeEvent$.subscribe((inputEvent: Event) => {
+            window.console.log(inputEvent.currentTarget['value']);
+            if (inputEvent.currentTarget['value'] === 'cancel') {
+                inputChangeEventSubScription.unsubscribe();
+            }
+        })
+    }
 }
