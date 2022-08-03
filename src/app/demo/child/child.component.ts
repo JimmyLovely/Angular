0c2754977a54015a2803182889ebd8172b629549
Jimmy Li <jimmy.li@elekta.com>
2022-08-03 17:21:33 +0800

    Basis: isFirstChange in ngChanges (#45)

--- src/app/demo/child/child.component.ts
+++ src/app/demo/child/child.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnChanges, OnInit, SimpleChanges } from '@angular/core';
 
 @Component({
     selector: 'app-child',
     templateUrl: './child.component.html',
     styleUrls: ['./child.component.scss']
 })
-export class ChildComponent implements OnInit {
+export class ChildComponent implements OnInit, OnChanges {
+    @Input() childSimpleValue: string = '';
 
     constructor() { }
 
     ngOnInit(): void {
     }
+
+    ngOnChanges(changes: SimpleChanges): void {
+        if (changes['childSimpleValue']) {
+            window.console.table(changes['childSimpleValue']);
+        }
+    }
+
 
 }
--- src/app/demo/parent/parent.component.ts
+++ src/app/demo/parent/parent.component.ts
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ParentComponent implements OnInit {
 
+    simpleValue: string = '';
+
     constructor() { }
 
     ngOnInit(): void {
+        this.simpleValue = '12';
     }
 
 }
